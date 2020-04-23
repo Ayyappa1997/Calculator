04c7bc18bf5717766ad69f12ce3b23ec7e0390ec
Ayyappa1997 <63583584+Ayyappa1997@users.noreply.github.com>
2020-04-23 18:58:31 +0530

    calling switch case statement from client side

--- Calculator/Calculate.cs
+++ Calculator/Calculate.cs
@@ -7,25 +7,21 @@ namespace Calculator
 {
     public class Calculate
     {
-        public double Operations(double inp1, double inp2, string type)
+       public double Add(double inp1, double inp2)
         {
-
-            switch(type)
-            {
-                case "Add":
-                    return inp1 + inp2;
-                  
-                case "Sub":
-                    return inp1 - inp2;
-                   
-                case "Mul":
-                    return inp1 * inp2;
-                    
-                case "Div":
-                    return inp1 / inp2;
-                default:
-                    return 0;
-            }
+            return inp1 + inp2;
         }
+        public double Sub(double inp1, double inp2)
+        {
+            return inp1 - inp2;
+        }
+        public double Mul(double inp1, double inp2)
+        {
+            return inp1 * inp2;
+        }
+        public double Div(double inp1, double inp2)
+        {
+            return inp1 / inp2;
+        }        
     }
 }
