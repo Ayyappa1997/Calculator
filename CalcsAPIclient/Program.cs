6354c61413e6cade7f300af908800b1b146090e3
Ayyappa1997 <63583584+Ayyappa1997@users.noreply.github.com>
2020-05-05 19:16:03 +0530

    Consuming WebApi from console application

---
+++ CalcsAPIclient/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace CalcsAPIclient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result;
+            Console.WriteLine("Please enter operand1");
+            double opr1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter operand2");
+            double opr2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please select the operation to perform");
+            Console.WriteLine(" 1.Add \n 2.Sub \n 3.Mul \n 4.Div");
+            string action = Console.ReadLine();
+            switch (action)
+            {
+                case "1":
+                    action = "Add";
+                    break;
+                case "2":
+                    action = "Sub";
+                    break;
+                case "3":
+                    action = "Mul";
+                    break;
+                case "4":
+                    action = "Div";
+                    break;
+                default:
+                    break;
+            }
+            var url = string.Format("{0}{1}{2}{3}{4}{5}", "http://localhost:63421/api/calculate/",action, "/", opr1,"/",opr2);
+
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add
+                (new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                string authInfo = Convert.ToBase64String(Encoding.Default.GetBytes("Ayyappa:Ayyappa97"));  
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authInfo);
+                var response = client.GetAsync(url).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = response.Content.ReadAsStringAsync().Result;
+                    result = Convert.ToDouble(data);
+                    Console.WriteLine("Result {0}", result);
+                }
+                else
+                {
+
+                    Console.WriteLine("server error");
+                }
+                
+                
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
---
+++ CalculatorAPI/Models/BasicAuthentication.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Web.Http.Description;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Security.Principal;
+
+namespace CalculatorAPI.Models
+{
+    public class BasicAuthentication:AuthorizationFilterAttribute
+    {      
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            if (actionContext.Request.Headers.Authorization==null)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+            }
+            else
+            {
+                string credentials = actionContext.Request.Headers.Authorization.Parameter;
+                string Dcredentials= Encoding.UTF8.GetString(Convert.FromBase64String(credentials));
+                string[] CredentialsArr = Dcredentials.Split(':');
+                string username = CredentialsArr[0];
+                string password = CredentialsArr[1];
+                if (Authentication.Verify(username, password))
+                {
+                    Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(username), null);
+                }
+                else
+                {
+                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                }
+            }
+        }
+
+       
+    }
+}
